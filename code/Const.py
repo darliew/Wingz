1657e382bf521e880599cd0c02fb3b75ec237bce
wjuniorbc <w.junior.barbosac@gmail.com>
2025-07-04 11:52:46 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,17 @@
+import pygame
+
+# C
+C_PURPLE = (160, 32, 240)
+C_BLUE = (60, 15, 160)
+C_BLACK = (0, 0, 0)
+
+
+# M
+MENU_OPTION = ('PLAY',
+               'SCORE',
+               'EXIT')
+
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/Menu.py
+++ code/Menu.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, MENU_OPTION, C_PURPLE, C_BLUE
+
 
 class Menu:
-    def __init__(self):
-        self.window = None
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+
+        while True:
+            # draw images
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(100, "Wingz", C_BLUE, ((WIN_WIDTH / 2), 70))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(40, MENU_OPTION[i], C_PURPLE, ((WIN_WIDTH / 2), 200 + 35 * i))
+
+            pygame.display.flip()
+
+            # check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # close window
+                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
